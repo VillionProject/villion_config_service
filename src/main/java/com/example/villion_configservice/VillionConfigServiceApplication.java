6b3b6500625b9446f4576dda4ff8e6bc18f44337
sesam-me <lsi6601@gmail.com>
2024-04-25 22:24:28 +0900

    first commit

---
+++ src/main/java/com/example/villion_configservice/VillionConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.villion_configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillionConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VillionConfigServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/villion_configservice/VillionConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.villion_configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillionConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
